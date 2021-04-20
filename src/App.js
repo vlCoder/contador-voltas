57afd4bd974e0352a9138456ed21ecc51fba8365
Vinicius <vinicius.camargo@fliper.app>
2021-04-20 18:03:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const MostraVoltas = (props) => {
+  return (
+    <p>{props.voltas}<br />voltas</p>
+  )
+}
+
+const MostraTempo = (props) => {
+  const tempo = props.tempo;
+  const minutos = Math.round(tempo / 60);
+  const segundos = tempo % 60;
+  const minutosStr = minutos < 10 ? '0' + minutos : minutos;
+  const segundosStr = segundos < 10 ? '0' + segundos : segundos;
+  return (
+    <p>{`${minutosStr}:${segundosStr}`}<br /> Tempo medio de voltas</p>
+  )
+}
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
 function App() {
+  const [numVoltas, setnumVoltas] = useState(0)
+  const [running, setRunning] = useState(false)
+  const [tempo, setTempo] = useState(0);
+
+  useEffect(() => {
+    let timer = null;
+    if (running) {
+      timer = setInterval(() => {
+        setTempo(old => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    }
+  }, [running]);
+
+  const toogleRunnig = () => {
+    setRunning(!running);
+  }
+
+  const increment = () => {
+    setnumVoltas(numVoltas + 1);
+  }
+  const decrement = () => {
+    setnumVoltas(numVoltas - 1)
+  }
+
+  const reset = () => {
+    setTempo(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MostraVoltas voltas={numVoltas} />
+      <Button onClick={increment} text="+" />
+      <Button onClick={decrement} text="-" />
+
+      {
+        numVoltas > 0 &&
+        <MostraTempo tempo={Math.round(tempo / numVoltas)} />
+      }
+
+      <Button onClick={toogleRunnig} text="Iniciar" />
+      <Button onClick={reset} text="Reniciar" />
     </div>
   );
 }
